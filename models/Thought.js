fa73cdd13f18be7914a60c1e529fdce73f7959ea
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-05-05 17:59:54 -0400

    reactionSchema in thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,11 +1,52 @@
 const { Schema, model, Types } = require("mongoose");
 
 //reaction field's subdocument schema in the Thought model
-const reactionSchema = new Schema({
-    
-})
-
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //getter method to format the timestamp
+      get: (createdAtValue) => dateFormat(createdAtValue),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 const thoughtSchema = new Schema({
+
+
+
     
-})
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Thought;
